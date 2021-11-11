7513ccffb613e6fa9779ceb76bec456029fb2416
javier-fraga <javierfraga1997@gmail.com>
2021-11-11 19:05:49 +0100

    initial commit

---
+++ GestionTienda/src/main/java/com/proyecto/GestionTienda/GestionTiendaApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.GestionTienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionTiendaApplication.class, args);
+	}
+
+}
---
+++ GestionTienda/src/test/java/com/proyecto/GestionTienda/GestionTiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.GestionTienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
